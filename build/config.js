78872a0b1f05552cbb236e7df2ca0bcbb4c704f8
slovebj <su2075@qq.com>
2016-10-30 05:05:06 +0800

    POPOVER

--- build/config.js
+++ build/config.js
@@ -1,28 +1,45 @@
 var Components = require('../components.json');
 var path = require('path');
+var dependencies = require('../package.json').dependencies;
+var fs = require('fs');
+
+var utilsList = fs.readdirSync(path.resolve(__dirname, '../src/utils'));
+var mixinsList = fs.readdirSync(path.resolve(__dirname, '../src/mixins'));
 var externals = {};
 
 Object.keys(Components).forEach(function(key) {
-  externals[`packages/${key}/index.js`] = `element-ui/lib/${key}`;
-  externals[`packages/${key}/style.css`] = `element-ui/lib/${key}/style.css`;
-  externals['main/utils/clickoutside'] = 'element-ui/lib/utils/clickoutside';
-  externals['main/utils/popper'] = 'element-ui/lib/utils/popper';
-  externals['main/utils/vue-popper'] = 'element-ui/lib/utils/vue-popper';
+  externals[`zxui/packages/${key}`] = `zxui/lib/${key}`;
+});
+Object.keys(dependencies).forEach(function(key) {
+  externals[key] = key;
+});
+externals['zxui/src/locale'] = 'zxui/lib/locale';
+
+utilsList.forEach(function(file) {
+  file = path.basename(file, '.js');
+  externals[`zxui/src/utils/${file}`] = `zxui/lib/utils/${file}`;
+});
+mixinsList.forEach(function(file) {
+  file = path.basename(file, '.js');
+  externals[`zxui/src/mixins/${file}`] = `zxui/lib/mixins/${file}`;
 });
 
 exports.externals = Object.assign({
-  vue: {
-    root: 'Vue',
-    commonjs: 'vue',
-    commonjs2: 'vue',
-    amd: 'vue'
-  }
+  vue: 'vue'
 }, externals);
 
 exports.alias = {
   main: path.resolve(__dirname, '../src'),
   packages: path.resolve(__dirname, '../packages'),
-  examples: path.resolve(__dirname, '../examples')
+  examples: path.resolve(__dirname, '../examples'),
+  zxui: path.resolve(__dirname, '../')
+};
+
+exports.vue = {
+  root: 'Vue',
+  commonjs: 'vue',
+  commonjs2: 'vue',
+  amd: 'vue'
 };
 
 exports.jsexclude = /node_modules|utils\/popper\.js|utils\/date.\js/;
