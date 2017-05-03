b7b1f9c1d2387b2b262f991c7fb0775087198a78
slovebj <su2075@qq.com>
2017-05-03 17:35:37 +0800

    首页修改

--- build/cooking.markcook.js
+++ build/cooking.markcook.js
@@ -2,31 +2,31 @@ var cooking = require('cooking');
 var path = require('path');
 
 cooking.set({
-  entry: './plugins/markcook/src/main.js',
-  dist: './plugins/markcook/build',
-  template: false,
-  devServer: {
-    port: 8085,
-    log: false,
-    publicPath: '/'
-  },
-  minimize: true,
-  extends: ['vue2'],
+    entry: './plugins/markcook/src/main.js',
+    dist: './plugins/markcook/build',
+    template: false,
+    devServer: {
+        port: 8085,
+        log: false,
+        publicPath: '/'
+    },
+    minimize: true,
+    extends: ['vue2'],
 });
 
 cooking.add('externals', {
-  vue: {
-    root: 'Vue',
-    commonjs: 'vue',
-    commonjs2: 'vue',
-    amd: 'vue'
-  },
-  vuex: {
-    root: 'Vuex',
-    commonjs: 'vuex',
-    commonjs2: 'vuex',
-    amd: 'vuex'
-  }
+    vue: {
+        root: 'Vue',
+        commonjs: 'vue',
+        commonjs2: 'vue',
+        amd: 'vue'
+    },
+    vuex: {
+        root: 'Vuex',
+        commonjs: 'vuex',
+        commonjs2: 'vuex',
+        amd: 'vuex'
+    }
 });
 
 module.exports = cooking.resolve();
